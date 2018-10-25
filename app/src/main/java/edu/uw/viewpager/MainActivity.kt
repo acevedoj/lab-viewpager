838f0d8e9083726ebdfe7450728094f23f351d10
acevedoj <acevedoj@uw.edu>
2018-10-24 23:44:07 -0700

    lab

--- app/src/main/java/edu/uw/viewpager/MainActivity.kt
+++ app/src/main/java/edu/uw/viewpager/MainActivity.kt
@@ -1,44 +1,80 @@
 package edu.uw.viewpager
 
 import android.os.Bundle
-import android.support.v4.app.FragmentTransaction
+import android.support.v4.app.*
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
+import android.widget.Button
 
-class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener {
+
+
+class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener, SearchFragment.OnSearchListener {
+
+    var searchFragment: SearchFragment? = null
+    var listFragment: MovieListFragment? = null
+    var detailsFragment: DetailFragment? = null
+    private lateinit var movieAdapter: PagerAdapter
+    private lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchFragment = SearchFragment.newInstance()
+
+        viewPager = findViewById(R.id.movies)
+
+        movieAdapter = MoviePagerAdapter(supportFragmentManager)
+
+        viewPager.adapter = movieAdapter
+
     }
 
-    //respond to search button clicking
-    fun handleSearchClick(v: View) {
-        val text = findViewById<View>(R.id.txt_search) as EditText
-        val searchTerm = text.text.toString()
-
-        val fragment = MovieListFragment.newInstance(searchTerm)
-
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.container, fragment, MOVIE_LIST_FRAGMENT_TAG)
-        ft.addToBackStack(null)
-        ft.commit()
-    }
 
     override fun onMovieSelected(movie: Movie) {
         val fragment = DetailFragment.newInstance(movie)
+        movieAdapter.notifyDataSetChanged()
+    }
 
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.container, fragment, MOVIE_DETAIL_FRAGMENT_TAG)
-        ft.addToBackStack(null) //remember for the back button
-        ft.commit()
+    override fun onSearchSubmitted(searchTerm: String) {
+        listFragment = MovieListFragment.newInstance(searchTerm)
+        movieAdapter.notifyDataSetChanged()
+        viewPager.currentItem = 1
     }
 
     companion object {
-
         private val TAG = "MainActivity"
         val MOVIE_LIST_FRAGMENT_TAG = "MoviesListFragment"
         val MOVIE_DETAIL_FRAGMENT_TAG = "DetailFragment"
+    }
+
+    private inner class MoviePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+        override fun getCount(): Int {
+            if (listFragment == null) {
+                return 1
+            } else if (detailsFragment == null) {
+                return 2
+            } else {
+                return 3
+            }
+        }
+
+        override fun getItem(position: Int): Fragment? {
+            if (position == 0) {
+                return searchFragment
+            } else if (position == 1) {
+                return listFragment
+            } else {
+                return detailsFragment
+            }
+        }
+
+        override fun getItemPosition(`object`: Any?): Int {
+            return PagerAdapter.POSITION_NONE
+        }
     }
 }
